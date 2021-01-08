b83600f992ce81da324e1eeefacc2fa0fab56fac
Thamiris Mylius Monteiro <thamirismylius@gmail.com>
2021-01-08 14:57:11 -0300

    Created POST /event

--- controllers/assignmentController.js
+++ controllers/assignmentController.js
@@ -2,6 +2,12 @@ async function getBalance(id) {
   return id;
 }
 
+async function postEvent(event) {
+  console.log(event);
+  return event;
+}
+
 export {
   getBalance,
+  postEvent
 };
--- routes/assignmentRouter.js
+++ routes/assignmentRouter.js
@@ -1,11 +1,19 @@
 import express from 'express';
-import { getBalance } from '../controllers/assignmentController.js';
+import { getBalance, postEvent } from '../controllers/assignmentController.js';
 
 const router = express.Router();
 
 router.get('/balance', async (req, res) => {
   try {
     res.send(await getBalance(req.query.account_id));
+  } catch (err) {
+    res.status(400).send(err.message);
+  }
+});
+
+router.post('/event', async (req, res) => {
+  try {
+    res.send(await postEvent(req.body));
   } catch (err) {
     res.status(400).send(err.message);
   }
